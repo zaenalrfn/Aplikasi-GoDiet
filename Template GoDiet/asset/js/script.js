e285188b00de303e19b0f94d3511b7f12b54e6be
zaenalrfn <ZAofficial24@gmail.com>
2023-01-06 20:53:47 +0700

    menambahkan popup logout

---
+++ Template GoDiet/asset/js/script.js
@@ -0,0 +1,9 @@
+// BAGIAN POPUP LOGOUT
+let popup = document.querySelector(".popup");
+
+const Logout = () => {
+  popup.style.display = "flex";
+};
+const Tidak = () => {
+  popup.style.display = "";
+};
