a6e822755785bea223435aa07200ed78f2ec6482
zaenalrfn <ZAofficial24@gmail.com>
2023-01-08 20:18:07 +0700

    Menambahkan halaman cek ideal tubuh

--- Template GoDiet/asset/js/script.js
+++ Template GoDiet/asset/js/script.js
@@ -3,7 +3,9 @@ let popup = document.querySelector(".popup");
 
 const Logout = () => {
   popup.style.display = "flex";
+  document.body.style.overflow = "hidden";
 };
 const Tidak = () => {
   popup.style.display = "";
+  document.body.style.overflow = "";
 };
