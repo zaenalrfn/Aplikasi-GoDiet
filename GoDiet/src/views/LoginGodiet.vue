35011649a8f981fa5965521f30440b70cf486ccc
zaenalrfn <ZAOfficial24@gmail.com>
2023-02-09 14:34:15 +0700

    login firebase dan popup logout

--- GoDiet/src/views/LoginGodiet.vue
+++ GoDiet/src/views/LoginGodiet.vue
@@ -1,3 +1,9 @@
+<style scoped>
+.box-login {
+  width: auto;
+}
+</style>
+
 <template>
   <div class="container">
     <div class="row text-center justify-content-center">
@@ -18,20 +24,30 @@
     </div>
   </div>
   <div class="row justify-content-center mt-4">
-    <div class="col text-center">
-      <GoogleLogin :callback="login" />
+    <div class="col text-center" @click="Login">
+      <!-- <GoogleLogin :callback="login" /> -->
+      <div
+        class="box-login d-flex align-items-center justify-content-center gap-3 bg-primary me-4 ms-4 rounded"
+      >
+        <div class="box">Lanjutkan Dengan Google</div>
+        <div class="box-img">
+          <img src="/img/login.png" alt="" :width="53.45" :height="54.36" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { decodeCredential } from "vue3-google-login";
-import { GoogleLogin } from "vue3-google-login";
+// import { decodeCredential } from "vue3-google-login";
+// import { GoogleLogin } from "vue3-google-login";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 export default {
   name: "Login",
-  components: {
-    GoogleLogin,
-  },
+  // components: {
+  //   GoogleLogin,
+  // },
   data() {
     return {
       getProfilImg: null,
@@ -39,17 +55,33 @@ export default {
     };
   },
   methods: {
-    login(response) {
-      // decodeCredential will retrive the JWT payload from the credential
-      let res = response;
-      const userData = decodeCredential(res.credential);
-      console.log("Handle the userData", userData);
-      this.getProfilImg = userData.name;
-      this.namaP = userData.picture;
-      localStorage.setItem("picture", this.namaP);
-      localStorage.setItem("name", this.getProfilImg);
-      localStorage.setItem("auth-login", true);
-      this.$router.push({ name: "home" });
+    // login(response) {
+    //   // decodeCredential will retrive the JWT payload from the credential
+    //   let res = response;
+    //   const userData = decodeCredential(res.credential);
+    //   console.log("Handle the userData", userData);
+    //   this.getProfilImg = userData.name;
+    //   this.namaP = userData.picture;
+    //   localStorage.setItem("picture", this.namaP);
+    //   localStorage.setItem("name", this.getProfilImg);
+    //   localStorage.setItem("auth-login", true);
+    //   this.$router.push({ name: "home" });
+    // },
+    async Login() {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(getAuth(), provider)
+        .then((result) => {
+          console.log(result.user);
+          this.getProfilImg = result.user.displayName;
+          this.namaP = result.user.photoURL;
+          localStorage.setItem("picture", this.namaP);
+          localStorage.setItem("name", this.getProfilImg);
+          localStorage.setItem("auth-login", true);
+          this.$router.push({ name: "home" });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
